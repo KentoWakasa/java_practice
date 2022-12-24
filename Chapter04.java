3fdcb36b57517d329684c382970d18ce2a14e168
KentoWakasa <103986115+KentoWakasa@users.noreply.github.com>
2022-12-24 10:54:23 +0900

    update chapter6

---
+++ Chapter04.java
@@ -0,0 +1,15 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String name;
+        final int LUCKY_NUM;
+        name = "Kento Wakasa";
+        LUCKY_NUM = 8;
+
+        System.out.println(name);
+        System.out.println(LUCKY_NUM);
+
+        name = "huhahaha";
+        System.out.println(name);
+    }
+    
+}
---
+++ Chapter05.java
@@ -0,0 +1,25 @@
+public class Chapter5 {
+    public static void main(String[] args) {
+        long a = 100L;
+        double b = 3.14;
+        boolean c = true;
+        
+        Float f = 1.41f;
+        Integer e = 100;
+        Character k = '笑';
+
+        int[] array1 = new int[3];
+        array1[0] = 10;
+        array1[1] = 20;
+        array1[2] = 30;
+        int int1 = array1[2];
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(k);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(int1);
+    }
+}
